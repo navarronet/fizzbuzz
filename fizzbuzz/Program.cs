497c9b754125aa264403d25b43ee04cbf1d539d0
Milton Navarro <milton.j.navarro.p@gmail.com>
2023-01-16 14:51:56 -0600

    Add project files.

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter a number: ");
+
+var userInput = Console.ReadLine();
+
+int num = int.Parse(userInput);
+
+if (num % 5 == 0 && num % 3 == 0)
+{
+    Console.WriteLine("FizzBuzz");
+} else if (num % 5 == 0)
+{
+    Console.WriteLine("Buzz");
+} else if (num % 3 == 0)
+{
+    Console.WriteLine("Fizz");
+} else
+{
+    Console.WriteLine("No FizzBuzz found.");
+}
